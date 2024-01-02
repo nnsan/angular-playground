f61211507165c8405c3426686ec9650cfe5e1fa6
San Nguyen Ngoc <sannguyen@nashtechglobal.com>
2024-01-02 17:03:01 +0700

    nested-form: implement payment.component

--- projects/nested-form/src/app/payment/payment.component.ts
+++ projects/nested-form/src/app/payment/payment.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../service/local-storage.service';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss']
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
+  paymentOption: string = 'creditCard';
+  product: string = '';
 
+  constructor(private localStorageService: LocalStorageService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.product = this.localStorageService.getData('productKey')?? '';
+  }
+
+  onSubmit() {
+    console.log(this.paymentOption);
+  }
 }
